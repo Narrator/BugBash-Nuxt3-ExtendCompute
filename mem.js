3e9e8aed8bddb6b045fb8c8c2b1b17773d6027e9
Kaushik Gnanaskandan <kgnanask@amazon.com>
2025-02-10 21:41:35 -0800

    Update mem.js

--- mem.js
+++ mem.js
@@ -1,2 +1,4 @@
+const os = require('os');
+
 console.log(`Process memory limit: ${process.memoryUsage().heapTotal / (1024 * 1024)} MB`);
 console.log(`System free memory: ${os.freemem() / (1024 * 1024)} MB`);
